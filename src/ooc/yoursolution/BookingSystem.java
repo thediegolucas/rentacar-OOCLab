9522f09799e4f2c520af7286723fbc08b13d0390
Diego Lucas <diegolucas@outlook.ie>
2021-11-03 13:11:49 +0000

    Update BookingSystem.java
    Added comments to the code.

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -21,10 +21,27 @@ public class BookingSystem implements BookingSystemInterface {
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
         
-        // Create an array list to store the cars
+        //Create an array list to store the cars from the txt document
         String st, name;
         List<Car> cars = new ArrayList<>();
         name = in.readLine();
 
+        //While loop to separate information in the txt document
+        while ((st = in.readLine()) != null) {
+            String s[] = st.split(":");
+
+            Make make = Make.valueOf(s[0]);
+            double rate = Double.parseDouble(s[1]);
+            int numCars = Integer.parseInt(s[2]);
+            
+            //For loop to check the number of cars
+            for (int i=0; i<numCars; i++) {
+                Car car = new Car(i, make, rate);
+                cars.add(car);
+            }
+        }
+
+        RentACarInterface rentACarInterface = new RentACar(cars, name);
+        return rentACarInterface;
     }
 }
