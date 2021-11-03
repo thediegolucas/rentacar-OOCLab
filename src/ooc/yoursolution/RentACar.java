4e1cecfd72534090ab825cfa0316c79642ad3bcf
Diego Lucas <diegolucas@outlook.ie>
2021-11-03 13:41:56 +0000

    Update RentACar.java

--- src/ooc/yoursolution/RentACar.java
+++ src/ooc/yoursolution/RentACar.java
@@ -9,6 +9,59 @@ package ooc.yoursolution;
  *
  * @author diegolucas
  */
-public class RentACar {
-    
-}
+import java.util.List;
+import ooc.enums.*;
+
+public class RentACar implements RentACarInterface {
+
+    private List<Car> cars;
+    private String name;
+
+    public RentACar(List<Car> cars, String name) {
+        this.cars = cars;
+        this.name = name;
+    }
+
+    @Override
+    public List getCars() {
+        return cars;
+    }
+
+    @Override
+    public void setCars(List cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean checkAvailability(Month month, int day, Make make, int lengthOfRent) {
+        int currentDay, flag;
+
+        for (Car car : cars) {
+            if (car.getMake().equals(make)) {
+                flag = 0;
+                currentDay = day;
+                for (int i=0; i<lengthOfRent; i++) {
+                    if (!car.isAvailable(month, currentDay++)) {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
